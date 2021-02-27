36f8fd0012ff68afb1fd52b1687b97fa305d9e87
olehkhalin <olehkhalin@gmail.com>
2021-02-28 01:27:38 +0200

    Test

--- src/utils/dapp.ts
+++ src/utils/dapp.ts
@@ -116,32 +116,33 @@ export const [
  * Block update
  */
 export const useOnBlock = (tezos: TezosToolkit, callback: (hash: string) => void) => {
-  // const blockHashRef = useRef<string | undefined>();
-  //
-  // useEffect(() => {
-  //   let sub: any; // Which type do I have to set here?
-  //
-  //   const spawnSub = () => {
-  //     sub = tezos.stream.subscribe('head');
-  //
-  //     sub.on('data', (hash: string) => {
-  //       if (blockHashRef.current && blockHashRef.current !== hash) {
-  //         callback(hash);
-  //       }
-  //       blockHashRef.current = hash;
-  //     });
-  //     sub.on('error', (err: Error) => {
-  //       if (process.env.NODE_ENV === 'development') {
-  //         console.error(err);
-  //       }
-  //       sub.close();
-  //       spawnSub();
-  //     });
-  //   };
-  //
-  //   spawnSub();
-  //   return () => sub.close();
-  // }, [tezos, callback]);
+  const blockHashRef = useRef<string | undefined>();
+
+  useEffect(() => {
+    console.log('useOnBlock2');
+    let sub: any; // Which type do I have to set here?
+
+    const spawnSub = () => {
+      sub = tezos.stream.subscribe('head');
+
+      sub.on('data', (hash: string) => {
+        if (blockHashRef.current && blockHashRef.current !== hash) {
+          callback(hash);
+        }
+        blockHashRef.current = hash;
+      });
+      sub.on('error', (err: Error) => {
+        if (process.env.NODE_ENV === 'development') {
+          console.error(err);
+        }
+        sub.close();
+        spawnSub();
+      });
+    };
+
+    spawnSub();
+    return () => sub.close();
+  }, [tezos, callback]);
 };
 /**
  * Block update
