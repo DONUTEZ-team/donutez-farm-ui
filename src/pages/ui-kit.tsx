de7043e6c5cc4c3cf0221fa8ba1d0cb9512d2b72
olehkhalin <olehkhalin@gmail.com>
2021-04-09 13:44:49 +0300

    Headings

--- src/pages/ui-kit.tsx
+++ src/pages/ui-kit.tsx
@@ -7,6 +7,7 @@ import { NextSeo } from 'next-seo';
 import { BaseLayout } from '@layouts/BaseLayout';
 import { Row } from '@components/ui/Row';
 import { Container } from '@components/ui/Container';
+import { Heading } from '@components/common/Heading';
 import { Button } from '@components/ui/Button';
 
 import s from '../styles/UiKit.module.sass';
@@ -26,65 +27,76 @@ const UiKit: React.FC = () => {
       />
       <Container>
         <Row className={s.row}>
-          <h1>[DEV] UI Kit</h1>
-          <div className={s.buttons}>
-            <div className={s.buttonsRow}>
-              <Button className={s.button}>
-                Primary
-              </Button>
-              <Button
-                disabled
-                className={s.button}
-              >
-                Primary
-              </Button>
-            </div>
-            <div className={s.buttonsRow}>
-              <Button
-                theme="secondary"
-                className={s.button}
-              >
-                Secondary
-              </Button>
-              <Button
-                theme="secondary"
-                disabled
-                className={s.button}
-              >
-                Secondary
-              </Button>
-            </div>
+          <div className={s.block}>
+            <h1 className={s.header}>[DEV] UI Kit</h1>
           </div>
-          <div className={cx(s.buttons, s.buttonsLight)}>
-            <div className={s.buttonsRow}>
-              <Button
-                theme="light"
-                className={s.button}
-              >
-                Light
-              </Button>
-              <Button
-                theme="light"
-                disabled
-                className={s.button}
-              >
-                Light
-              </Button>
+          <div className={s.block}>
+            <Heading header="Headings" subheader="01" />
+            <Heading header="Required" subheader="01" />
+            <Heading header="Optional header" subheader="02" isRequired={false} />
+            <Heading header="Create it!" subheader="Finally" isRequired={null} />
+          </div>
+          <div className={s.block}>
+            <Heading header="Buttons" subheader="02" />
+            <div className={s.buttons}>
+              <div className={s.buttonsRow}>
+                <Button className={s.button}>
+                  Primary
+                </Button>
+                <Button
+                  disabled
+                  className={s.button}
+                >
+                  Primary
+                </Button>
+              </div>
+              <div className={s.buttonsRow}>
+                <Button
+                  theme="secondary"
+                  className={s.button}
+                >
+                  Secondary
+                </Button>
+                <Button
+                  theme="secondary"
+                  disabled
+                  className={s.button}
+                >
+                  Secondary
+                </Button>
+              </div>
             </div>
-            <div className={s.buttonsRow}>
-              <Button
-                theme="lightSecondary"
-                className={s.button}
-              >
-                Light Secondary
-              </Button>
-              <Button
-                theme="lightSecondary"
-                disabled
-                className={s.button}
-              >
-                Light Secondary
-              </Button>
+            <div className={cx(s.buttons, s.buttonsLight)}>
+              <div className={s.buttonsRow}>
+                <Button
+                  theme="light"
+                  className={s.button}
+                >
+                  Light
+                </Button>
+                <Button
+                  theme="light"
+                  disabled
+                  className={s.button}
+                >
+                  Light
+                </Button>
+              </div>
+              <div className={s.buttonsRow}>
+                <Button
+                  theme="lightSecondary"
+                  className={s.button}
+                >
+                  Light Secondary
+                </Button>
+                <Button
+                  theme="lightSecondary"
+                  disabled
+                  className={s.button}
+                >
+                  Light Secondary
+                </Button>
+              </div>
             </div>
           </div>
         </Row>
